cf4adc1b43c61107ed1c1ca240dc721ca4427bcc
Felipe Azevedo <felipe.azevedo@cws.digital>
2024-06-28 16:14:22 -0300

    project initialized

---
+++ src/main/java/com/felipeazsantos/api_personal_finance/ApiPersonalFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.felipeazsantos.api_personal_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPersonalFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPersonalFinanceApplication.class, args);
+	}
+
+}
